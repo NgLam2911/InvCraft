a802629c749464d45bc5ed53152199b2e8c935fd
Ngọc Lam <33188123+NgLamVN@users.noreply.github.com>
2020-12-31 15:20:57 +0700

    Everything is WIP

---
+++ src/NgLamVN/InvCraft/command/InvCraftCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NgLamVN\InvCraft\command;
+
+use pocketmine\command\PluginCommand;
+use pocketmine\plugin\Plugin;
+
+class InvCraftCommand extends PluginCommand
+{
+    //TODO : Command
+}
