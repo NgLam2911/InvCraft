a5f5495b2d9c01161c9d477107d8adf5e1a687e0
Ngọc Lam <nglamvn2911@gmail.com>
2024-04-08 11:58:32 +0700

    First Look at rewrite

---
+++ src/NgLam2911/InvCraft/InvCraft.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace NgLam2911\InvCraft;
+
+use muqsit\invmenu\InvMenuHandler;
+use pocketmine\plugin\PluginBase;
+
+class InvCraft extends PluginBase{
+
+    protected function onEnable(): void{
+        if (!InvMenuHandler::isRegistered()) {
+            InvMenuHandler::register($this);
+        }
+
+    }
+}
