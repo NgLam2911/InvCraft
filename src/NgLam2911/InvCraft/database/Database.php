78697b6c50056e0a2159c5c6ca0048153ffe5113
Ngọc Lam <nglamvn2911@gmail.com>
2024-04-09 14:52:13 +0700

    Lmfao composer

--- src/NgLam2911/InvCraft/database/Database.php
+++ src/NgLam2911/InvCraft/database/Database.php
@@ -5,7 +5,7 @@ namespace NgLam2911\InvCraft\database;
 
 use Generator;
 use NgLam2911\InvCraft\InvCraft;
-use NgLam2911\InvCraft\libs\_9fbc4bfefe0cd102\SOFe\AwaitGenerator\Await;
+use SOFe\AwaitGenerator\Await;
 use NgLam2911\InvCraft\recipe\Recipe;
 use NgLam2911\InvCraft\utils\NbtHelper;
 use poggit\libasynql\DataConnector;
